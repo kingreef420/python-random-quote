ab249f30898499ddcc9862174b459faa902441be
kingreef420 <95227767+kingreef420@users.noreply.github.com>
2022-04-16 18:14:49 -0400

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def name():
+def primary():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -7,5 +7,5 @@ def name():
 
   #print(quotes)
 
-if __name__== "__main__":
- name()
+if __nprimaryame__== "__main__":
+ primary()
